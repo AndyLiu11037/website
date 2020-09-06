40c8f5bfdb1866dac13aa0a5b16837fd8f23d95f
Andy Liu <andyliu11037@gmail.com>
2020-09-06 17:14:11 +1000

    page intro work

--- src/Home.js
+++ src/Home.js
@@ -2,35 +2,34 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar.js';
+import Intro from './Components/Intro.js';
 import {hot} from "react-hot-loader";
 import 'animate.css';
-import anime from 'animejs'
+import anime from 'animejs';
+
 class Home extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			value: null,
+			renderIntro: true,
 		};
+        this.interval = null;
 	}
 	componentDidMount(){
-		anime({
-			targets: '.test',
-			translateX:'13rem',
-			rotate:180,
-			borderRadius:'8px',
-			loop:true,
-			duration: 4000
-		});
+		this.interval = setTimeout(() => this.setState({renderIntro: false}),7000);
 	}
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}   
 	render(){
 		return (
 			<div>
 				<React.Fragment>
-
+					{ this.state.renderIntro ? <Intro /> : null }
 					<div> {/*make a div wrapper for fade in the whole page 
 							When using svg line drawing animation, need to add strok="colour"*/}
 					<NavBar/>
-
+					
 					
 					<div className="website-name animate__animated animate__bounce">Andy Liu's Resume</div>
 					<div className="title-text">
