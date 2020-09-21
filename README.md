0295389867a10ad38b6781f4ed91618eb063fb9d
Andy Liu <andyliu11037@gmail.com>
2020-09-21 23:05:12 +1000

    add in about section

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # website TODO list 
-- [ ] Think about website/background design
-- [ ] add in about section into home page
-- [ ] change css for interests 
+- [x] Think about website/background design
+- [x] add in about section into home page
+- [ ] change css for interests
+- [ ] change css for about 
 - [ ] add project link to home page
 - [ ] add projects to portfolio page
 - [ ] have a link to updating resume
-- [ ] add light dark mode
 - [ ] fix css so its universal on mobile and browser
 - [ ] change css colours also svg stroke colours
--- src/Home.js
+++ src/Home.js
@@ -17,6 +17,14 @@ class Home extends React.Component {
 							<p className="title-2">Software Developer</p>
 						</div>
 					</div>
+					<div className="about">
+						<p className="title">About Me</p>
+						<p className="text">I'm a recent graduate from the University of New South Wales studying Computer Science. I have quite a few interests 
+											that I pursue in my spare time relating to tech and other things. I'm always learning new things from doing my own projects, 
+											whether it be a pure programming project, Arduino projects or even just following tutorials to further my understanding in new areas.
+											Feel free to contact me about opportunities or to have a chat through <a className="text" href="https://www.linkedin.com/in/andy-liu-375aa117b/">LinkedIn</a> or my email:andyliu11037@gmail.com.
+											 </p>
+					</div>
 					<div className="website-name animate__animated animate__bounce">Andy Liu's Resume</div>
 					<br></br>
 					<div className="interest">
@@ -25,6 +33,7 @@ class Home extends React.Component {
 							<li>Software Development</li>
 							<li>Cyber Security</li>
 							<li>Neural networks kinda</li>
+							<li>Psychology</li>
 						</ul>
 					</div>
 					<div className="portfolio">
