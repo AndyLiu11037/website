fbc64d58bdb1476c3036f0deecf7c3bef61f0cf7
Andy Liu <andyliu11037@gmail.com>
2020-10-07 17:11:19 +1100

    css change for sections to include one time fadein

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 - [x] Think about website/background design
 - [x] add in about section into home page
 - [x] change css for interests
-- [ ] change css for about 
+- [x] change animation so it only happens once
+- [x] change css for about 
+- [ ] fix sizes and fonts to viewport height/width
 - [ ] add project link to home page
 - [ ] add projects to portfolio page
 - [ ] have a link to updating resume
-- [ ] fix sizes and fonts to viewport height/width
+
 - [ ] fix css so its universal on mobile and browser
 - [ ] change css colours also svg stroke colours
 - [ ] change interest section so its longer
--- src/Home.js
+++ src/Home.js
@@ -7,69 +7,108 @@ class Home extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			interestVisibility: 'hidden',
+			interestVisibility: 'invisible',
+			aboutVisibility: 'invisible',
+			portfolioVisibility: 'invisible',
 		}
-		this.handleChange = this.handleChange.bind(this);
+		this.handleInterest = this.handleInterest.bind(this);
+		this.handleAbout = this.handleAbout.bind(this);
+		this.handlePortfolio = this.handlePortfolio.bind(this);
 	}
-	handleChange(event){
-		console.log(event);
-		this.setState({
-			interestVisibility: event.isIntersecting ? 'visible' : 'invisible',
-		});
+
+	handleAbout(event){
+		if (this.state.aboutVisibility==='invisible') {
+			this.setState({
+				aboutVisibility: event.isIntersecting ? 'visible' : 'invisible',
+			});
+		}
+	};
+
+	handleInterest(event){
+		if (this.state.interestVisibility==='invisible') {
+			this.setState({
+				interestVisibility: event.isIntersecting ? 'visible' : 'invisible',
+			});
+		}
 	};
 	
-	render(){
-		const options = {
-			onChange: this.handleIntersection,
-			root: 'title-page',
-			rootMargin:''
+	handlePortfolio(event){
+		//console.log(event);
+		console.log(this.state.portfolioVisibility);
+		if (this.state.portfolioVisibility==='invisible') {
+			this.setState({
+				portfolioVisibility: event.isIntersecting ? 'visible' : 'invisible',
+			});
 		}
+	};
+
+	render(){
 		return (
 				<div className="title-page">{/*
 							When using svg line drawing animation, need to add strok="colour"*/}
-
-						
-						
-					
 					<div className="title-screen">
 						<div className="home-title">
 							<p className="title">I'm Andy Liu</p>
 							<p className="title-2">Software Developer</p>
 						</div>
 					</div>
-					<div className="about">
-						<p className="title">About Me</p>
-						<p className="text">I'm a recent graduate from the University of New South Wales studying Computer Science. I have quite a few interests 
-											that I pursue in my spare time relating to tech and other things. I'm always learning new things from doing my own projects, 
-											whether it be a pure programming project, Arduino projects or even just following tutorials to further my understanding in new areas.
-											Feel free to contact me about opportunities or to have a chat through <a className="text" href="https://www.linkedin.com/in/andy-liu-375aa117b/">LinkedIn</a> or my email:andyliu11037@gmail.com.
-											 </p>
-					</div>
-					<Observer onChange={this.handleChange}>
+					<Observer onChange={this.handleAbout}>
+					<br></br>
+					</Observer>
+					{
+						this.state.aboutVisibility==='visible' 
+						?
+						<div className="about-section">
+							<div className="about animate__animated animate__fadeInLeft">
+								<p className="title">About Me</p>
+								<p className="text">I'm a recent graduate from the University of New South Wales studying Computer Science. I have quite a few interests 
+													that I pursue in my spare time relating to tech and other things. I'm always learning new things from doing my own projects, 
+													whether it be a pure programming project, Arduino projects or even just following tutorials to further my understanding in new areas.
+													Feel free to contact me about opportunities or to have a chat through <a className="text" href="https://www.linkedin.com/in/andy-liu-375aa117b/">LinkedIn</a> or my email:andyliu11037@gmail.com.
+								</p>
+							</div>
+						</div>
+
+						:
+						null
+					}	
+					<Observer onChange={this.handleInterest}>
 					<br></br>
 					</Observer>
 					{
 						this.state.interestVisibility==='visible' 
-						? 						
-						<div className="interest animate__animated animate__fadeInLeft">
-								<p className="title">Interests</p>
-								<ul className="text">
-									<li>Software Development</li>
-									<li>Cyber Security</li>
-									<li>Neural networks kinda</li>
-									<li>Psychology</li>
-								</ul>
-						</div>
+						?
+						<div className="interest-section"> 
+							<div className="interest animate__animated animate__fadeInLeft">
+									<p className="title">Interests</p>
+									<ul className="text">
+										<li>Software Development</li>
+										<li>Cyber Security</li>
+										<li>Neural networks kinda</li>
+										<li>Psychology</li>
+									</ul>
+							</div>
+						</div> 						
 						: 
 						null
 					}
-
+					<Observer onChange={this.handlePortfolio}>
+					<br></br>
+					</Observer>
+					{
+						this.state.portfolioVisibility==='visible'
+						?
+						<div className="portfolio-section animate__animated animate__fadeInLeft">
 							<div className="portfolio">
 								<p className="title">Portfolio</p>
 								<p className="text"> My Personal Projects</p>
 							</div> 
-							
-					<div className="website-name animate__animated animate__fadeInLeft">Andy Liu's Resume</div>
+						</div>
+						:
+						null
+					}
+
+
 				</div>
 			
 		);
